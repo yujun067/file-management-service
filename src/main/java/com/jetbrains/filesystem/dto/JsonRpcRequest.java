01bdefcd3ece3628670b996a15c4e8db1b4526c4
Yu Jun <yujun067@gmail.com>
2025-07-01 14:35:44 +0200

    feature: initialize Spring Boot project skeleton with JSON-RPC endpoint scaffold and basic config

--- src/main/java/com/jetbrains/filesystem/dto/JsonRpcRequest.java
+++ src/main/java/com/jetbrains/filesystem/dto/JsonRpcRequest.java
@@ -1,7 +1,6 @@
 package com.jetbrains.filesystem.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class JsonRpcRequest {
